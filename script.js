13100cee769e975a77bbc0d4aec09ce1c6599961
Jacob Cohn <jacobnc916@gmail.com>
2021-04-22 01:32:42 -0500

    finished making hover effect and clear button

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 const gridContainer = document.querySelector('#gridContainer');
 
 // a is the number of columns and rows
-let a = 16;
+let a = 64;
 
 gridContainer.style.cssText = `
     grid-template-columns: repeat(` + a + `, 1fr);
 `;
 
-
+// creates all of the divs for the grid
 for (let i = 0; i < (a ** 2); i++) {
-    const grid = document.createElement('div');
-    gridContainer.appendChild(grid);
-    grid.style.cssText = 'background: white';
+    const oneGrid = document.createElement('div');
+    gridContainer.appendChild(oneGrid);
+    oneGrid.style.cssText = 'background: white';
+    oneGrid.classList.toggle('one-grid');
 }
+
+const allGrids = document.querySelectorAll('.one-grid');
+
+// allGrids will change colors when mouse enters oneGrid
+allGrids.forEach(item => {
+    item.addEventListener('mouseenter', e => {
+        e.target.style.cssText = 'background: black';
+    });
+});
+
+// allGrids will change colors to white
+// when clearBtn is clicked
+const clearBtn = document.querySelector('#clearBtn');
+clearBtn.addEventListener('click', () => {
+    allGrids.forEach(item => {
+        item.style.cssText = 'background: white';
+    });
+});
